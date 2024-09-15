dc02cf620f8f3d65facf7e527fa578b3547263a8
Games Unblocked <Brainmassage@outlook.com>
2024-09-16 02:02:21 +0300

    aw

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-(window.screenOrientation = 'sensor_landscape'),
-	// loadLib('./libs/min/laya.core.min.js'),
-	// loadLib('./libs/min/laya.ani.min.js'),
-	// loadLib('./libs/min/laya.ui.min.js'),
-	// loadLib('./libs/min/laya.d3.min.js'),
-	// loadLib('./libs/min/jc.box.min.js'),
-	// loadLib('./libs/min/laya-zip.min.js'),
-	// loadLib('./libs/min/laya.physics3D.min.js'),
-	// loadLib('./js/bundle.js');
-
-
-
-
-
-    loadLib('https://h5gamessdk.yyggames.com/sdk/laya/2.13.2beta/laya.core.js'),
-	loadLib('https://h5gamessdk.yyggames.com/sdk/laya/2.13.2beta/laya.ani.js'),
-	loadLib('https://h5gamessdk.yyggames.com/sdk/laya/2.13.2beta/laya.ui.js'),
-	loadLib('https://h5gamessdk.yyggames.com/sdk/laya/2.13.2beta/laya.d3.js'),
-	loadLib('./libs/min/jc.box.min.js'),
-	// loadLib('./libs/min/laya-zip.min.js'),
-	loadLib('https://h5gamessdk.yyggames.com/sdk/laya/2.13.2beta/laya.physics3D.js'),
-	loadLib('./js/main.js');
+window.screenOrientation = "sensor_landscape", loadLib("libs/laya.core.js"), loadLib("libs/laya.ui.js"),
+    loadLib("libs/laya.d3.js"), loadLib("libs/jslinq.min.js"), loadLib("js/bundle_.js");
